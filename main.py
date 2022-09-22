5381fe1fe54910c6d0202f1703e0d36b8c01e288
Lukas Bühler <mail@lukasbuehler.ch>
2022-09-22 10:41:32 +0200

    further progress

--- main.py
+++ main.py
@@ -5,36 +5,31 @@ import base64
 import datetime
 import json
 
-session = requests.Session()
+
+def _authenticate(token):
+    session = requests.Session()
+    #print("Token successfully loaded")
+    api_url = "https://api.track.toggl.com/api/v9/me/sessions" # curl -u 1971800d4d82861d8f2c1651fea4d212:api_token
+    
+    auth_base64_string = base64.b64encode(f"{token}:api_token".encode('ascii')).decode('ascii')
+    
+    response = session.post(api_url, headers={'Authorization': f'Basic {auth_base64_string}'})
+    
+    if response.status_code != requests.codes.ok:
+        response.raise_for_status()
+        return None
+
+    return session
 
 
-def _authenticate():
-    load_dotenv()
-
-    tokens = os.getenv("TOGGL_API_TOKEN", "").split(",")
-
-    for token in tokens:
-        #print("Token successfully loaded")
-        api_url = "https://api.track.toggl.com/api/v9/me/sessions" # curl -u 1971800d4d82861d8f2c1651fea4d212:api_token
-        
-        auth_base64_string = base64.b64encode(f"{token}:api_token".encode('ascii')).decode('ascii')
-        
-        response = session.post(api_url, headers={'Authorization': f'Basic {auth_base64_string}'})
-        
-        if response.status_code != requests.codes.ok:
-            response.raise_for_status()
-
-
-def _get_project_name_by_project_id(workspace_id, project_id):
+def _get_project_name_by_project_id(session, workspace_id, project_id):
     response = session.get(f"https://api.track.toggl.com/api/v9/workspaces/{workspace_id}/projects/{project_id}")
     
     if response.status_code == requests.codes.ok:
-        print(response.json())
-
         return response.json()["name"]
 
 
-def _get_time_entries_of_today():
+def _get_time_entries_of_today(session):
     today = datetime.date.today()
     tomorrow = datetime.date.today() + datetime.timedelta(days=1)
 
@@ -51,9 +46,12 @@ def _get_time_entries_of_today():
         response.raise_for_status()
         return
 
-def _get_hours_today(time_entries=None):
+
+def _get_hours_today(session, time_entries=None):
     if not time_entries:
-        time_entries = _get_time_entries_of_today()
+        time_entries = _get_time_entries_of_today(session)
+
+    print(time_entries)
 
     hours_by_projects = {}
 
@@ -68,16 +66,28 @@ def _get_hours_today(time_entries=None):
             project_id = str(project_id)
             if project_id not in hours_by_projects:
                 workspace_id = time_entry["workspace_id"]
-                project_name = _get_project_name_by_project_id(workspace_id, project_id)
+                project_name = _get_project_name_by_project_id(session, workspace_id, project_id)
                 hours_by_projects[project_id] = { "name": project_name,"workspace_id": workspace_id, "hours": 0 }
 
-            if time_entry["duration"] > 0:
+            if time_entry["stop"]:
                 hours_by_projects[project_id]["hours"] += time_entry["duration"] / 3600
+            else:
+                start_datetime_string = time_entry["start"]
+                start_datetime = datetime.datetime.fromisoformat(start_datetime_string)
+                hours_by_projects[project_id]["hours"] += (datetime.now - start_datetime).total_seconds() / 3600
+            
 
     print(hours_by_projects)
         
         
 
 if __name__ == "__main__":
-    _authenticate()
-    _get_hours_today()
+    load_dotenv()
+
+    tokens = os.getenv("TOGGL_API_TOKEN", "").split(",")
+
+    for token in tokens:
+        session = _authenticate(token)
+
+        if session:
+            _get_hours_today(session)
