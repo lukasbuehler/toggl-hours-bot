149d8f918ad215857f18f2d13680fd1e14fa9ebc
Lukas Bühler <me@lukasbuehler.ch>
2023-08-07 21:28:57 +0200

    commented out error handler

--- telegram_handler.py
+++ telegram_handler.py
@@ -21,6 +21,7 @@ logging.basicConfig(
 )
 logger = logging.getLogger(__name__)
 
+
 def start_bot():
     token = os.getenv("TELEGRAM_BOT_TOKEN", "")
 
@@ -28,16 +29,16 @@ def start_bot():
         print("Starting Telegram bot")
         app = ApplicationBuilder().token(token).build()
 
-        app.add_handler(CommandHandler('today', _send_hours_chart_today))
-        app.add_handler(CommandHandler('day', _send_hours_chart_today))
-        app.add_handler(CommandHandler('yesterday', _send_hours_chart_yesterday))
-        app.add_handler(CommandHandler('week', _send_hours_chart_week))
-        app.add_handler(CommandHandler('lastweek', _send_hours_chart_lastweek))
-        app.add_handler(CommandHandler('month', _send_hours_chart_month))
-        app.add_handler(CommandHandler('lastmonth', _send_hours_chart_lastmonth))
-        app.add_handler(CommandHandler('semester', _send_hours_chart_semester))
+        app.add_handler(CommandHandler("today", _send_hours_chart_today))
+        app.add_handler(CommandHandler("day", _send_hours_chart_today))
+        app.add_handler(CommandHandler("yesterday", _send_hours_chart_yesterday))
+        app.add_handler(CommandHandler("week", _send_hours_chart_week))
+        app.add_handler(CommandHandler("lastweek", _send_hours_chart_lastweek))
+        app.add_handler(CommandHandler("month", _send_hours_chart_month))
+        app.add_handler(CommandHandler("lastmonth", _send_hours_chart_lastmonth))
+        app.add_handler(CommandHandler("semester", _send_hours_chart_semester))
 
-        app.add_error_handler(error_handler)
+        # app.add_error_handler(error_handler) # dsabled for now
 
         app.run_polling()
 
@@ -52,7 +53,9 @@ async def error_handler(update: object, context: ContextTypes.DEFAULT_TYPE) -> N
 
         # traceback.format_exception returns the usual python message about an exception, but as a
         # list of strings rather than a single string, so we have to join them together.
-        tb_list = traceback.format_exception(None, context.error, context.error.__traceback__)
+        tb_list = traceback.format_exception(
+            None, context.error, context.error.__traceback__
+        )
         tb_string = "".join(tb_list)
 
         # Build the message with some markup and additional information about what happened.
@@ -76,22 +79,24 @@ async def error_handler(update: object, context: ContextTypes.DEFAULT_TYPE) -> N
 def _get_image_bytes_from_file_path(path):
     if not path:
         print("image path is empty!")
-        return 
+        return
 
-    with open(path, 'rb') as image:
+    with open(path, "rb") as image:
         f = image.read()
-        #b = bytearray(f)
+        # b = bytearray(f)
         return f
 
 
 async def _send_hours_chart(hours_type, update, context) -> str:
     chat_id = update.message.chat_id
 
-    #print(f"Chat ID: {chat_id}")
+    # print(f"Chat ID: {chat_id}")
     path, data = generate_hours_chart(hours_type)
     img_bytes = _get_image_bytes_from_file_path(path)
 
-    message = await context.bot.send_photo(chat_id=chat_id, photo=img_bytes, reply_to_message_id=update.message.message_id)
+    message = await context.bot.send_photo(
+        chat_id=chat_id, photo=img_bytes, reply_to_message_id=update.message.message_id
+    )
 
     return message.message_id
 
@@ -99,50 +104,61 @@ async def _send_hours_chart(hours_type, update, context) -> str:
 async def _send_hours_chart_today(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await _send_hours_chart("today", update, context)
 
+
 async def _send_hours_chart_yesterday(update, context):
     await _send_hours_chart("yesterday", update, context)
+
 
 async def _send_hours_chart_week(update, context):
     await _send_hours_chart("week", update, context)
 
+
 async def _send_hours_chart_lastweek(update, context):
     await _send_hours_chart("lastweek", update, context)
+
 
 async def _send_hours_chart_month(update, context):
     await _send_hours_chart("month", update, context)
 
+
 async def _send_hours_chart_lastmonth(update, context):
     await _send_hours_chart("lastmonth", update, context)
 
+
 async def _send_hours_chart_semester(update, context):
     await _send_hours_chart("semester", update, context)
-    
+
 
 def send_image_in_telegram_message(image_path, chat_id, token, caption) -> None:
     """
     Send an image with a caption from a local path to a specified chat.
-    
+
     Returns the message id of the message that was sent.
     """
     bot = Bot(token)
     loop = asyncio.get_event_loop()
 
-    with open(image_path, 'rb') as image:
-        loop.run_until_complete(bot.send_photo(chat_id=chat_id, photo=image, caption=caption))
-        
+    with open(image_path, "rb") as image:
+        loop.run_until_complete(
+            bot.send_photo(chat_id=chat_id, photo=image, caption=caption)
+        )
+
     loop.run_until_complete(bot.shutdown())
 
     loop.close()
 
+
 async def pin_message(token, chat_id, message_id):
     updater = Updater(token)
 
-    await updater.bot.pin_chat_message(chat_id=chat_id, message_id=message_id, disable_notification=True)
+    await updater.bot.pin_chat_message(
+        chat_id=chat_id, message_id=message_id, disable_notification=True
+    )
 
 
 if __name__ == "__main__":
     load_dotenv()
-    
+
     # start telegram bot
     if len(sys.argv) > 1 and str(sys.argv[1]) == "start":
         start_bot()
